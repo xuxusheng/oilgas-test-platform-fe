a6417c6a6d24db33921492f8498cb5f96992d12e
John <20691718@qq.com>
2025-12-16 00:21:05 +0800

    fix: 添加 Ant Design App 组件包裹以解决 message 警告
    - 在 ConfigProvider 内添加 App 组件
    - 使 message API 能够访问主题上下文
    - 解决 'Static function can not consume context like dynamic theme' 警告

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ConfigProvider } from 'antd'
+import { ConfigProvider, App } from 'antd'
 import zhCN from 'antd/locale/zh_CN'
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
@@ -17,7 +17,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConfigProvider locale={zhCN}>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
+        <App>
+          <RouterProvider router={router} />
+        </App>
       </QueryClientProvider>
     </ConfigProvider>
   </React.StrictMode>,
